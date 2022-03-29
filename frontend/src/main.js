3dea953fbf32ab8926e4925da0c38138a6b8308a
Caliton <calitongoncalves@landix.com.br>
2022-03-29 01:36:07 -0300

    feat: configure i18n for language english and portuguese

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,14 +4,23 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from './plugins/axios'
+import VueI18n from 'vue-i18n'
+import locales from './locales/locales.js'
 
 Vue.config.productionTip = false
 
 axios({ Vue })
 
+const i18n = new VueI18n({
+  locale: 'pt',
+  fallbackLocale: 'pt',
+  messages: locales
+})
+
 new Vue({
   router,
   store,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
