bd448e0fe0057bf45c62b872eb1897bb0f662495
Caliton <calitongoncalves@landix.com.br>
2022-03-29 01:53:23 -0300

    chore: add labels resource language

--- frontend/src/locales/en.js
+++ frontend/src/locales/en.js
@@ -1,5 +1,45 @@
 
 const en = {
+  // A
+  actions: 'Actions',
+
+  // C
+  cancel: 'cancel',
+  CPFInvalid: 'CPF invalid',
+
+  // E
+  errorNameMust: 'Name must be at most 70 characters long',
+  errorRaMustSix: 'RA must be at most 6 characters long.',
+  exclude: 'Exclude',
+  excludeStudent: 'Are you sure you want to exclude this student?',
+  editStudents: 'Edit Student',
+
+  // F
+  feelFreeToManage: 'Feel free to manage student registration.',
+
+  // H
+  home: 'Home',
+
+  // I
+  invalid: 'Invalid',
+
+  // M
+  mustEmail: 'Must be valid e-mail',
+
+  // N
+  name: 'Name',
+  newStudent: 'New Student',
+
+  // R
+  required: 'Required field',
+
+  // S
+  save: 'Save',
+  search: 'Search...',
+  students: 'Students',
+
+  // T
+  technicalChallenge: 'Technical challenge for +A Education.'
 
 }
 
--- frontend/src/locales/pt.js
+++ frontend/src/locales/pt.js
@@ -1,5 +1,46 @@
 const pt = {
 
+  // A
+  actions: 'Ações',
+
+  // C
+  cancel: 'cancelar',
+  CPFInvalid: 'CPF Inválido',
+
+  // E
+  excludeStudent: 'Tem certeza de que deseja excluir este aluno?',
+  exclude: 'Excluir',
+  editStudents: 'Editar Aluno',
+  errorNameMust: 'Nome pode conter até 70 caracteres',
+  errorRaMustSix: 'RA pode conter até 6 caracteres',
+
+  // F
+  feelFreeToManage: 'Sinta-se a vontade para gerir o cadastro de alunos.',
+
+  // H
+  home: 'Inicio',
+
+  // I
+  invalid: 'Inválido',
+
+  // M
+  mustEmail: 'Apenas e-mail válido',
+
+  // N
+  name: 'Nome',
+  newStudent: 'Novo Aluno',
+
+  // R
+  required: 'Campo obrigátorio',
+
+  // S
+  save: 'Salvar',
+  search: 'Buscar...',
+  students: 'Alunos',
+
+  // T
+  technicalChallenge: 'Desafio tecnico para +A Educação.'
+
 }
 
 export default pt
