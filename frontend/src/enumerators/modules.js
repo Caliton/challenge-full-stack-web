2675c57ff0127622a3b4b7cdfc58973d4a489f34
Caliton <calitongoncalves@landix.com.br>
2022-03-29 01:41:25 -0300

    chore: scaled modules for json

---
+++ frontend/src/enumerators/modules.js
@@ -0,0 +1,12 @@
+export const modules = [
+  {
+    link: 'home',
+    label: 'home',
+    icon: 'mdi-view-dashboard'
+  },
+  {
+    link: 'students',
+    label: 'students',
+    icon: 'mdi-school'
+  }
+]
